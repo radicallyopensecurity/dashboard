86ada8a9de073ef3a066e03c50dd2c33f9aab794
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-01-20 15:10:20 +0000

    show recent activity in projects list

--- elements/ros/projects.js
+++ elements/ros/projects.js
@@ -1,7 +1,16 @@
+import moment from '../../web_modules/moment.js';
 import { LitElement, html, css } from '../../web_modules/lit-element.js';
 import { GitlabProjects } from '../gitlab/index.js';
 
 class Projects extends GitlabProjects {
+
+	static get styles() {
+		return css`
+		.small {
+			font-size: 0.75em;
+		}
+		`;
+	}
 
 	render() {
 		return html`
@@ -14,6 +23,9 @@ class Projects extends GitlabProjects {
 				${this.projects.map((project) => html`
 					<li>
 						<a href="#${project.id}">${project.name}</a>
+						<span class="small">
+							${moment(project.last_activity_at).fromNow()}
+						</span>
 					</li>
 				`)}
 			</ul>
