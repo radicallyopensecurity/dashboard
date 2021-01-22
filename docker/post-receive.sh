a1d7c142a389a1cf30a2b22541c1f92746f8c85a
Stefan Grönke <stefan@gronke.net>
2021-01-22 17:19:59 +0100

    print branch info in git post-receive hook

--- docker/post-receive.sh
+++ docker/post-receive.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-GIT_BRANCH="$(git rev-parse --symbolic --abbrev-ref $1)"
-TARGET_DIR="/var/www/html/$GIT_BRANCH"
-
-echo "ROS Dashboard builder"
-
-mkdir -p "$TARGET_DIR"
-
-docker run --rm \
-	-v "$(pwd):/source:ro" \
-        -v "$TARGET_DIR:/target:rw" \
-        -e REV="$1" \
-        dashboard-builder
+while read oldrev newrev refname
+do
+	GIT_BRANCH="$(git rev-parse --symbolic --abbrev-ref $refname)"
+	TARGET_DIR="/var/www/html/$GIT_BRANCH"
+	
+	echo "ROS Dashboard builder"
+	echo "Rev: $refname"
+	echo "Branch: $GIT_BRANCH"
+	echo "Target directory: $TARGET_DIR"
+	
+	mkdir -p "$TARGET_DIR"
+	
+	docker run --rm \
+		-v "$(pwd):/source:ro" \
+	        -v "$TARGET_DIR:/target:rw" \
+	        -e REV="$1" \
+	        dashboard-builder
+done
