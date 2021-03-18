8d65fa2962bce645afb0ac3a5e2d819d619db8b9
Johann Derdak <johann@derdak.de>
2021-03-18 13:45:42 +0100

    add breadcrumb for project view

--- elements/ros/project.js
+++ elements/ros/project.js
@@ -292,8 +292,17 @@ export class Project extends GitlabProject {
 				</nav>
 
 				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-					<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-						<h1>${this.gitlabProjectData.name_with_namespace}</h1>
+					<div class="w-100 pt-3">
+						<nav aria-label="breadcrumb mb-1">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href=".">Projects</a></li>
+								<li class="breadcrumb-item"><a href="https://git.staging.radical.sexy/groups/ros" target="_blank">${this.gitlabProjectData.namespace.name}</a></li>
+								<li class="breadcrumb-item active" aria-current="page">${this.gitlabProjectData.name}</li>
+							</ol>
+						</nav>
+					</div>
+					<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+						<h1>${this.title}</h1>
 						<div class="btn-toolbar mb-2 mb-md-0">
 							<div class="btn-group me-2">
 								<button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
