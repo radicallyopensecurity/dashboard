8eea915958ba48621a4eb39d09737bd9b573e04a
Johann Derdak <johann@derdak.de>
2021-03-18 13:57:45 +0100

    change project title to project name in project view

--- elements/ros/project.js
+++ elements/ros/project.js
@@ -302,10 +302,10 @@ export class Project extends GitlabProject {
 						</nav>
 					</div>
 					<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-						<h1>${this.title}</h1>
+						<h1>${this.gitlabProjectData.name}</h1>
 						<div class="btn-toolbar mb-2 mb-md-0">
 							<div class="btn-group me-2">
-								<button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+								<button type="button" class="btn btn-sm btn-outline-secondary">Report</button>
 								<button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
 							</div>
 						</div>
