c0c9d68f9b8929a4fe9f4a9585d2bafab3d7d7d2
Johann Derdak <johann@derdak.de>
2021-03-18 12:04:12 +0100

    Change navigation link to all projects

--- elements/router.js
+++ elements/router.js
@@ -57,8 +57,7 @@ class Router extends LitElement {
 		<link rel="stylesheet" href="dashboard.css"/>
 		<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 			<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" 
-			   href="https://www.radicallyopensecurity.com/" 
-			   target="_blank">
+			   href=".">
 				Radically Open Security
 			</a>
 			<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
