e8f9cff6e53257f3de88e8a5b245dd6eb4ec309a
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-03-21 16:14:30 +0100

    break out DropdownInput

---
+++ elements/ui/input/dropdown.js
@@ -0,0 +1,109 @@
+import { LitElement, html } from '../../../web_modules/lit-element.js';
+import { LitNotify } from '../../../web_modules/@morbidick/lit-element-notify.js';
+
+export class DropdownInput extends LitNotify(LitElement) {
+
+	constructor() {
+		super();
+		this.path = undefined;
+		this.params = {};
+		this.value = undefined;
+		this._options = [];
+	}
+
+	static get properties() {
+		return {
+			path: {
+				type: String
+			},
+			params: {
+				type: Object
+			},
+			_options: {
+				type: Object,
+				notify: true
+			},
+			value: {
+				type: String,
+				notify: true
+			}
+		}
+	}
+
+	get options() {
+		return this._options;
+	}
+
+	set options(options) {
+		this._options = options;
+	}
+
+	get url() {
+		if (this.path == undefined) {
+			return;
+		}
+		const url = new URL(this.path, window.location.toString());
+		Object.entries(this.params).forEach(([key, value]) => url.searchParams.append(key, value));
+		return url;
+	}
+
+	async updated(changedProperties) {
+		const keys = [...changedProperties.keys()];
+		if (keys.includes("path") || keys.includes("params")) {
+			this.options = await this.query();
+		}
+		if (keys.includes("_options") && !keys.includes("value")) {
+			if ((this.value === undefined) && (this.options.length > 0)) {
+				this.value = this.options[0].value
+			}
+		}
+	}
+
+	async query(url) {
+		if (url === undefined) {
+			url = this.url;
+		}
+		if (url === undefined) {
+			return;
+		} else {
+			const response = await fetch(url.toString());
+			const data = await response.json();
+
+			switch (response.status) {
+				case 401:
+				case 403:
+					alert(data.message);
+					break;
+				case 400:
+					alert(data.message.name);
+					break;
+			}
+
+			return data.map(this.constructor.mapOptions);
+		}
+	}
+
+	static mapOptions(item) {
+		return item;
+	}
+
+	get onChangeSelection() {
+		return (e) => {
+			e.stopPropagation();
+			this.value = e.currentTarget.value;
+		}
+	}
+
+	render() {
+		return html`
+		<link rel="stylesheet" href="style.css"/>
+		<select @change="${this.onChangeSelection}">
+			${this.options.map((option) => {
+				const label = option.label || option.value;
+				return html`<option value="${option.value}" .selected="${option.value === this.value}">${label}</option>`;
+			})}
+		</select>
+		`;
+	}
+
+}
