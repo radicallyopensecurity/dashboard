9c11a331758aabd6dedccb72df0e21d1d7789c30
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-03-21 13:02:25 +0100

    adjust sidebar width on large screens
    Co-Authored-By: Johann Derdak <johannderdak@radicallyopensecurity.com>

--- elements/router.js
+++ elements/router.js
@@ -63,7 +63,7 @@ class Router extends LitElement {
 		<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css"/>
 		<link rel="stylesheet" href="dashboard.css"/>
 		<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-			<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" 
+			<a class="navbar-brand col-md-3 col-xl-2 me-0 px-3"
 			   href=".">
 				Radically Open Security
 			</a>
--- elements/views/allProjectsView.js
+++ elements/views/allProjectsView.js
@@ -30,7 +30,7 @@ class AllProjectsView extends LitElement {
 
 		<div class="container-fluid">
 			<div class="row">
-				<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+				<nav id="sidebarMenu" class="col-md-3 col-xl-2 d-md-block bg-light sidebar collapse">
 					<div class="position-sticky pt-3">
 						<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
 							<span>Cross links</span>
@@ -51,7 +51,7 @@ class AllProjectsView extends LitElement {
 						</ul>
 					</div>
 				</nav>
-				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+				<main class="col-md-9 col-xl-10 ms-sm-auto px-md-4">
 					<ros-projects
 						.params=${{search: "pen-", order_by: "last_activity_at"}}
 						.search="${this.search}"
