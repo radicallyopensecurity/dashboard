5016fec3949d7d33400c1f78d3dbce73966ff2f7
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-03-23 11:44:52 +0100

    add quick actions to all projects view

--- elements/views/allProjectsView.js
+++ elements/views/allProjectsView.js
@@ -33,6 +33,17 @@ class AllProjectsView extends LitElement {
 				<nav id="sidebarMenu" class="col-md-3 col-xl-2 d-md-block bg-body sidebar collapse">
 					<div class="position-sticky pt-3">
 						<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+							<span>Actions</span>
+						</h6>
+						<ul class="nav flex-column">
+							<li class="nav-item">
+								<a class="nav-link" aria-current="page" href="#new">
+									<ui-icon icon="plus-square"></ui-icon>
+									Start New Project
+								</a>
+							</li>
+						</ul>
+						<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
 							<span>ROS Services</span>
 						</h6>
 						<ul class="nav flex-column">
