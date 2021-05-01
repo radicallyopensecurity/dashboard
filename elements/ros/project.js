5737cbfab131d111382d17da0567a3c36854e961
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-05-01 13:56:00 +0200

    style finding preview code blocks

--- elements/ros/project.js
+++ elements/ros/project.js
@@ -20,6 +20,35 @@ marked.use({
 		heading(text, level) {
 			level += headingLevelOffset;
 			return `<h${level}>${text}</h${level}>`;
+		},
+		code(code, infostring, escaped) {
+			const lang = (infostring || '').match(/\S*/)[0];
+			if (this.options.highlight) {
+				const out = this.options.highlight(code, lang);
+				if (out != null && out !== code) {
+					escaped = true;
+					code = out;
+				}
+			}
+
+			code = code.replace(/\n$/, '') + '\n';
+
+			const $pre = document.createElement("pre");
+			const $code = document.createElement("code");
+			$pre.appendChild($code);
+			$pre.classList.add("bg-light", "p-3");
+
+			if (escaped) {
+				$code.innerHTML = code;
+			} else {
+				$code.innerText = code;
+			}
+
+			if (lang) {
+				$code.classList.add(`${this.options.langPrefix}${escape(lang, true)}`);
+			}
+
+			return $pre.outerHTML;
 		}
 	}
 });
