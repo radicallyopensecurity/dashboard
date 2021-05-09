c5534cf6a30da3ee49a8352e8661179003cb490e
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-05-09 06:31:59 +0200

    explicitly set container-fluid padding

--- elements/ros/overview.js
+++ elements/ros/overview.js
@@ -82,7 +82,7 @@ class Overview extends GitlabProjects {
 		return html`
 		<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css"/>
 
-		<div class="container-fluid">
+		<div class="container-fluid px-sm-0">
 			<div class="row">
 				<div class="col-12 bg-light">
 					<ui-content-card>
