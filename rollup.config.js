9d26202b1a97e978116f221fddac6748f6d26d04
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-08-12 01:18:18 +0200

    chore: upgrade snowpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [
+    {
+        input: "node_modules/moment/moment.js",
+        output: {
+            file: "web_modules/moment.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/marked/src/marked.js",
+        output: {
+            file: "web_modules/d3.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/lit/index.js",
+        output: {
+            file: "web_modules/lit.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/lit-html/directives/style-map.js",
+        output: {
+            file: "web_modules/lit-html/directives/style-map.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/lit-html/directives/class-map.js",
+        output: {
+            file: "web_modules/lit-html/directives/class-map.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/lit-html/directives/repeat.js",
+        output: {
+            file: "web_modules/lit-html/directives/repeat.js"
+        },
+        plugins: [nodeResolve()]
+    },{
+        input: "node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js",
+        output: {
+            file: "web_modules/@webcomponents/webcomponentsjs.js"
+        }
+    }
+]
