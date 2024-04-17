4b5a3713fadbe8d1d14496ec505fd619aab61bdf
syrosh <syrosh@radicallyopensecurity.com>
2024-04-17 15:56:16 +0200

    fix(pdf-password): generate only gitlab allowed characters in password

--- elements/utils/generate-password.js
+++ elements/utils/generate-password.js
@@ -1,6 +1,8 @@
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBERS = "0123456789";
-const SYMBOLS = `!@#$%^&*()[]{};:'\`"<>,.?/\|-_+=~`;
+// not all special characters are supported in GL when masking
+// https://git.staging.radical.sexy/help/ci/variables/index#mask-a-cicd-variable
+const SYMBOLS = "@:.~-+/=";
 
 const PASSWORD_CHARACTERS = [
   ALPHABET.toUpperCase(),
@@ -14,14 +16,14 @@ const PASSWORD_CHARACTERS = [
  * @param {number} length
  * @returns string
  */
-export const generatePassword = (length = 20) => {
+export const generatePassword = (length = 20, characters = PASSWORD_CHARACTERS) => {
   let password = "";
 
   // Monte Carlo
   while (password.length < length) {
     const array = new Uint8Array(1);
     const [randomInt] = crypto.getRandomValues(array);
-    const char = PASSWORD_CHARACTERS[randomInt];
+    const char = characters[randomInt];
     if (char) {
       password += char;
     }
