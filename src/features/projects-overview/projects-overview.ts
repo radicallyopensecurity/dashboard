08cffc1c64954ffa00bc7c3f8a9884db8c6013bb
syrosh <syrosh@radicallyopensecurity.com>
2024-06-14 07:36:56 +0200

    feat(home): basic table

--- src/features/projects-overview/projects-overview.ts
+++ src/features/projects-overview/projects-overview.ts
@@ -1,16 +1,22 @@
 import { SignalWatcher } from '@lit-labs/preact-signals'
-import { format } from 'date-fns'
+import { ColumnDef } from '@tanstack/lit-table'
 import { LitElement, html, css } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import { map } from 'lit/directives/map.js'
+import { customElement, property, state } from 'lit/decorators.js'
 
 import { theme } from '@/theme/theme'
 
 import { Project } from '@/modules/projects/types/project'
 
-import { addChannelNames } from '@/modules/projects/utils/add-channel-names'
+import {
+  ProjectWithChannelNames,
+  addChannelNames,
+} from '@/modules/projects/utils/add-channel-names'
+
+import { createColumns } from './columns'
 
 import { chatSubscriptionsSignal } from '@/modules/chat/signals/chat-subscriptions-signal'
+
+import '@/elements/data-table/data-table'
 
 const ELEMENT_NAME = 'projects-overview'
 
@@ -18,6 +24,9 @@ const ELEMENT_NAME = 'projects-overview'
 export class ProjectsOverview extends SignalWatcher(LitElement) {
   @property()
   projects: Project[] = []
+
+  @state()
+  columns: ColumnDef<ProjectWithChannelNames>[] = createColumns(new Date())
 
   static styles = [
     ...theme,
@@ -35,63 +44,11 @@ export class ProjectsOverview extends SignalWatcher(LitElement) {
     )
 
     return html`<sl-card>
-      <h2>Projects</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Status</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Last GitLab Activity</th>
-            <th>Last Quote Channel Activity</th>
-            <th>Last Pentest Channel Activity</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${map(
-            projectsWithChannels,
-            ({ project, pentestChannel, quoteChannel }) =>
-              html`<tr>
-                <td>
-                  <a href=${`/projects/${project.pathWithNamespace}`}
-                    >${project.pathWithNamespace}</a
-                  >
-                </td>
-                <td>
-                  <sl-badge
-                    pill
-                    variant=${project.isPentest ? 'danger' : 'warning'}
-                    >${project.isPentest ? 'pentest' : 'quote'}</sl-badge
-                  >
-                </td>
-                <td>${project.status}</td>
-                <td>
-                  ${project.startDate
-                    ? format(project.startDate, 'yyyy-MM-dd')
-                    : 'TBD'}
-                </td>
-                <td>
-                  ${project.endDate
-                    ? format(project.endDate, 'yyyy-MM-dd')
-                    : 'TBD'}
-                </td>
-                <td>${format(project.lastActivityAt, 'yyyy-MM-dd')}</td>
-                <td>
-                  ${quoteChannel
-                    ? format(quoteChannel.lastUpdatedAt, 'yyyy-MM-dd')
-                    : 'unknown'}
-                </td>
-                <td>
-                  ${pentestChannel
-                    ? format(pentestChannel.lastUpdatedAt, 'yyyy-MM-dd')
-                    : 'unknown'}
-                </td>
-              </tr>`
-          )}
-        </tbody>
-      </table>
+      <h2>Overview</h2>
+      <data-table
+        .columns=${this.columns}
+        .data=${projectsWithChannels}
+      ></data-table>
     </sl-card> `
   }
 }
